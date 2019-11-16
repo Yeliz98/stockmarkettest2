c144b6e75544d49016d3311faa2ef322a490a38d
Yeliz98 <yelizoe98@hotmail.de>
2019-11-16 22:41:55 +0100

    4 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StockItemComponent } from './stock-item/stock-item.component';
+import { CreateStockComponent } from './create-stock/create-stock.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StockItemComponent ],
+  declarations: [ AppComponent, HelloComponent, StockItemComponent, CreateStockComponent ],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
