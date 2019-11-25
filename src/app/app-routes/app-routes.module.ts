5d866bc7e8dd517574720be5934043ab04ca4cc4
Yeliz98 <yelizoe98@hotmail.de>
2019-11-25 09:42:27 +0100

    1 file modified

--- src/app/app-routes/app-routes.module.ts
+++ src/app/app-routes/app-routes.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from '../user/login/login.component';
 import { RegisterComponent } from '../user/register/register.component';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'stocks/list', component: StockListComponent },
